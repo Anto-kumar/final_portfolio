159a8807de4a0d76435bbba7f1263495787378d7
anto <antokumar00@gmail.com>
2024-03-04 12:39:33 +0600

    First

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using portfolio.Data;
+using portfolio.Models;
+
+namespace portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ApplicationDbContext _db;
+        public ContactController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact obj)
+        {
+            _db.Contacts.Add(obj);
+            _db.SaveChanges();
+            return View();
+        }
+    }
+}
